1b38f17b9252eb4e5c83bd6f801b23a02f7a8e2b
Yash Jhaveri <yashj65@gmail.com>
2020-09-09 19:53:30 +0530

    added styling

--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,29 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Launch from './Launch';
+import {Grid,Card,AppBar,Toolbar,Typography} from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+      display: "flex",
+      justifyContent: 'center',
+  },
+}));
 
 const Launches = () => {
+  const classes = useStyles();
+
   const [launches, setLaunches] = useState([]);
 
   useEffect(() => {
-    axios.get('https://api.spacexdata.com/v3/launches/?limit=5')
+    axios.get('https://api.spacexdata.com/v3/launches/?limit=9')
         .then(response => setLaunches(response.data))
         .catch(err => console.error(err));
   }, []);
 
   return (
-    <div className="App">
-      <h1>SPACEX</h1>
-      <div className="launches">
-      {launches.map(launch => (
-        <Launch 
-            key={launch.flight_number}
-            id={launch.flight_number}
-            name={launch.mission_name}
-            year={launch.launch_year}
-            image={launch.links.mission_patch}
-        />
-        ))}
+    <div className="App" style={{background: "black"}}>
+      <div style={{marginBottom: '20px'}}>
+        <AppBar position="static">
+              <Toolbar className={classes.navbar}>
+                  <Typography variant="h3">SpaceX</Typography>
+              </Toolbar>
+        </AppBar>
+      </div>
+      <div className="Grid"  style={{marginLeft: '20px',marginRight: '20px'}}>
+        <Grid container spacing={2} justify="flex-start" alignItems="flex-start">
+          {launches.map(launch => (
+              <Grid item xs={4}>
+                <Card className="Card">
+                    <Launch 
+                      key={launch.flight_number}
+                      id={launch.flight_number}
+                      name={launch.mission_name}
+                      year={launch.launch_year}
+                      image={launch.links.mission_patch}
+                  />
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
       </div>
     </div>
   );
