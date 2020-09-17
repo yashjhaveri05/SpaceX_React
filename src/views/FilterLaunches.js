c2eccb0422bbcfa045dff1344c9165d4e0f0641c
Yash Jhaveri <yashj65@gmail.com>
2020-09-17 09:21:53 +0530

    FilterLaunches bug fix

--- src/views/FilterLaunches.js
+++ src/views/FilterLaunches.js
@@ -13,22 +13,18 @@ const useStyles = makeStyles((theme) => ({
       marginRight: 50,
       textAlign: "center"
   },
-  form: {
-    marginBottom: 35,
-    marginTop: 35,
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
   field : {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
+    marginTop: 30,
     width: 300,
   },
   button: {
-    marginTop: 10
+    marginTop: 30
+  },
+  button1: {
+    marginTop: 30,
+    marginLeft: 10
   },
   grid: {
     marginLeft: 50,
@@ -38,12 +34,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const FilterLaunches = ({setFiltered}) => {
-    const classes = useStyles();
-
-  const [search, setSearch] = useState("");
-  const [two, setTwo] = useState("");
-  const [query, setQuery] = useState("2017-03-03");
-  const [qutwo, setQutwo] = useState("2017-03-04");
+  const classes = useStyles();
+  const [query, setQuery] = useState("");
+  const [qutwo, setQutwo] = useState("");
 
   const getFiltered = async() => {
       const response = await fetch(`https://api.spacexdata.com/v3/launches/?start=${query}&end=${qutwo}`);
@@ -51,35 +44,16 @@ const FilterLaunches = ({setFiltered}) => {
       await setFiltered(data);
   }
 
-
-  const updateSearch = e => {
-    setSearch(e.target.value);
-  };
-
-  const updateTwo = e => {
-    setTwo(e.target.value);
-  };
-
-  const getSearch = e => {
-    e.preventDefault();
-    setQuery(search);
-    setQutwo(two);
-    setSearch("");
-    setTwo("");
-    getFiltered();
-  };
-
   return (
     <div className="FilterLaunches">
      <main>
-      <form onSubmit={getSearch} className={classes.form}>
       <Grid container spacing={2} justify="center" alignItems="center">
           <TextField
               id="date1"
               type="date"
               label="Start Date:"
-              defaultValue={search}
-              onChange={updateSearch}
+              value={query}
+              onChange={e => setQuery(e.target.value)}
               color="primary"
               className={classes.field}
               InputLabelProps={{
@@ -90,17 +64,17 @@ const FilterLaunches = ({setFiltered}) => {
               id="date2"
               type="date"
               label="End Date:"
-              defaultValue={two}
-              onChange={updateTwo}
+              value={qutwo}
+              onChange={e => setQutwo(e.target.value)}
               color="primary"
               className={classes.field}
               InputLabelProps={{
               shrink: true,
               }}
           />
-         <Button size="large" variant="contained" color="secondary" type="submit" className={classes.button}>Search</Button>
+         <Button onClick={getFiltered} size="large" variant="contained" color="secondary" className={classes.button}>Search</Button>
+         <Button onClick={() => window.location.reload()} size="large" variant="contained" color="secondary" className={classes.button1}>Back</Button>
       </Grid>
-      </form>
      </main>
     </div>
   );
